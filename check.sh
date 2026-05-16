269addc2ca17a9a4a505aa6a45378474e8f354a0
Andrej Kolčin <self@kaathewise.net>
2026-05-16 22:19:28 +0300

    check.sh: split tests into light ones and `--full`
    The latter tests all combinations of features while the former only runs
    checks with individual features enabled to save time.

--- check.sh
+++ check.sh
@@ -1,17 +1,38 @@
 set -e
 
-FEATURE_SETS="none half zstd lz4 zstd,lz4 zstd,lz4,half"
+FEATURES="half zstd lz4"
+
+FEATURE_SETS="none $FEATURES"
+
+for arg in "$@"; do
+    if [ "$arg" = "--full" ]; then
+        # Generate powerset for --full
+        FEATURE_SETS="none"
+        for f in $FEATURES; do
+            NEW_COMBS=""
+            for c in $FEATURE_SETS; do
+                if [ "$c" = "none" ]; then
+                    NEW_COMBS="$NEW_COMBS $f"
+                else
+                    NEW_COMBS="$NEW_COMBS ${c},${f}"
+                fi
+            done
+            FEATURE_SETS="$FEATURE_SETS $NEW_COMBS"
+        done
+        break
+    fi
+done
 
 cargo fmt --check
 
 for features in $FEATURE_SETS; do
     if [ "$features" = "none" ]; then
-        cargo check --no-default-features --tests
-        cargo clippy --no-default-features --tests
-        cargo test --no-default-features
+        FEATURE_ARGS=""
     else
-        cargo check --no-default-features --tests --features "$features"
-        cargo clippy --no-default-features --tests --features "$features"
-        cargo test --no-default-features --features "$features"
+        FEATURE_ARGS="--features $features"
     fi
+
+    cargo check --no-default-features --tests $FEATURE_ARGS
+    cargo clippy --no-default-features --tests $FEATURE_ARGS
+    cargo test --no-default-features $FEATURE_ARGS
 done
