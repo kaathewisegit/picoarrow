01c2ba0d6c02bbe3584f757fbfab3d36a4ad61fd
Andrej Kolčin <self@kaathewise.net>
2026-05-16 20:29:38 +0300

    check.sh: check tests too

--- check.sh
+++ check.sh
@@ -4,12 +4,12 @@ FEATURE_SETS="none zstd lz4 zstd,lz4"
 
 for features in $FEATURE_SETS; do
     if [ "$features" = "none" ]; then
-        cargo check --no-default-features
-        cargo clippy --no-default-features
+        cargo check --no-default-features --tests
+        cargo clippy --no-default-features --tests
         cargo test --no-default-features
     else
-        cargo check --no-default-features --features "$features"
-        cargo clippy --no-default-features --features "$features"
+        cargo check --no-default-features --tests --features "$features"
+        cargo clippy --no-default-features --tests --features "$features"
         cargo test --no-default-features --features "$features"
     fi
 done
